46a333138c5e21f0e0dc88ec0646189a42d58a2c
sugan0tech <sugankpms@gmail.com>
2023-01-18 15:17:35 +0530

    fixed the recipeService mocking issues

--- src/test/java/com/recipe/project/recipe/controller/RecipeControllerTest.java
+++ src/test/java/com/recipe/project/recipe/controller/RecipeControllerTest.java
@@ -1,6 +1,7 @@
 package com.recipe.project.recipe.controller;
 
 import com.recipe.project.recipe.models.Recipe;
+import com.recipe.project.recipe.repositories.CategoryRepository;
 import com.recipe.project.recipe.services.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,18 +14,23 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.concurrent.TimeUnit;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class RecipeControllerTest {
     @Mock
     RecipeService recipeService;
+
+    @Mock
+    CategoryRepository categoryRepository;
 
     RecipeController controller;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.initMocks(this);
-        controller = new RecipeController(recipeService);
+        controller = new RecipeController(recipeService, categoryRepository);
     }
 
     @Test
@@ -33,9 +39,22 @@ class RecipeControllerTest {
         Recipe recipe = new Recipe();
         recipe.setId(1L);
 
+        when(recipeService.getRecipeById(1L)).thenReturn(recipe);
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
         mockMvc.perform(get("/recipe/1/show"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("recipe/show"));
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+
+    @Test
+    @Timeout(value = 1000, unit =  TimeUnit.MILLISECONDS)
+    void newRecipe() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(get("/recipe/new"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/recipeform"))
+                .andExpect(model().attributeExists("recipe"))
+                .andExpect(model().attributeExists("categories"));
     }
 }
